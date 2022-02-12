2fd61516e27afcfceab07a989aa816e04b34ba5d
Stepan Melnickov <stepan.melnickov.ne@yandex.ru>
2022-02-12 17:40:55 +0500

    Метка сохраняется на карте

--- MapApp.py
+++ MapApp.py
@@ -10,13 +10,22 @@ class MapApp(PyQt5.QtWidgets.QMainWindow):
         super(MapApp, self).__init__()
         uic.loadUi('map5.ui', self)
         self.show()
+        self.add_params = dict()
         self.centre = f"{coord1},{coord2}"
         self.z = z
         self.show_map()
         self.search_button.clicked.connect(self.show_object)
 
-    def show_map(self, add_params=None):
-        img = show_map(self.centre, self.z, add_params=add_params)
+    def show_map(self):
+        mode = self.map_mode.currentText()
+        if mode == 'Схема':
+            mode = 'map'
+        elif mode == 'Гибрид':
+            mode = 'sat,skl'
+        elif mode == 'Спутник':
+            mode = 'sat'
+
+        img = show_map(self.centre, self.z, map_type=mode, add_params=self.add_params)
         pixmap = PyQt5.QtGui.QPixmap()
         pixmap.loadFromData(QByteArray(img))
         self.map_label.setPixmap(pixmap)
@@ -37,14 +46,17 @@ class MapApp(PyQt5.QtWidgets.QMainWindow):
     def show_object(self):
         ll, spn = get_ll_spn(self.search_line.text())
         self.centre = ll
-        self.show_map({"pt": f"{ll},pm2rdm"})
+        if 'pt' in self.add_params:
+            self.add_params['pt'] = f"{self.add_params}~{f'{ll},pm2rdm'}"
+        else:
+            self.add_params["pt"] = f"{ll},pm2rdm"
 
 
 if __name__ == '__main__':
     app = PyQt5.QtWidgets.QApplication(sys.argv)
-    # ll, spn = get_ll_spn('Ветлужская 89')
-    # map_app = MapApp(*ll.split(','), 14)
-    map_app = MapApp(sys.argv[1], sys.argv[2], sys.argv[3])
+    ll, spn = get_ll_spn('Ветлужская 89')
+    map_app = MapApp(*ll.split(','), 14)
+    # map_app = MapApp(sys.argv[1], sys.argv[2], sys.argv[3])
     map_app.show()
     sys.exit(app.exec())
     #  sys.argv[1], sys.argv[2], sys.argv[3]
